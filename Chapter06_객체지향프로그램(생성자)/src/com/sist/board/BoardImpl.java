a5c6a21a8f0247320edfeeef4817b69d66e73c2d
sist <sist@sist27>
2025-08-13 17:26:20 +0900

    16

---
+++ Chapter06_객체지향프로그램(생성자)/src/com/sist/board/BoardImpl.java
@@ -0,0 +1,13 @@
+package com.sist.board;
+
+public class BoardImpl implements Board1 {
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!!");
+	}
+
+
+
+}
