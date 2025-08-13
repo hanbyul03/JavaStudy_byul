09a42261c3fe06686ae3dfbe17acd63e86d3d63d
sist <sist@sist27>
2025-08-13 15:36:05 +0900

    5

---
+++ Chapter03_제어문(조건문,선택문)/src/제어문_조건문7.java
@@ -0,0 +1,18 @@
+/*
+ *   점수를 입력 => 0~100사이 
+ *   = 60이상이면 합격
+ *   = 60미만 불합격 
+ *   
+ *   if(score>=60)
+ *      합격
+ *   if(score<60)
+ *     불합격 
+ */
+public class 제어문_조건문7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
