042a10a6122dbd74500e85cbc539f80a1c0a5e0d
sist <sist@sist27>
2025-08-13 12:40:43 +0900

    3

---
+++ Chapter02_변수와타입/src/변수문자열_52.java
@@ -0,0 +1,15 @@
+
+public class 변수문자열_52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Java 13 => 파이썬 => 53page 
+        String news="""
+        		축구 통계 전문 사이트 '후스코어드닷컴'은 최근 2024-2025시즌을 기준으로 유럽 5대 리그(잉글랜드 프리미어리그, 스페인 라리가, 독일 분데스리가, 이탈리아 세리에 A, 프랑스 리그1)에서 1500분 이상 출전한 선수들을 대상으로 '90분당 빅 찬스 생성' 통계를 집계했다.
+
+그 결과, 이강인은 90분당 0.86회의 빅 찬스를 만든 것으로 나타났으며, 이 부문 전체 5위에 올랐다.
+        		""";
+        System.out.println(news);
+	}
+
+}  
