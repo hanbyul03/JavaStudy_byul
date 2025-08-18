4eff0450283a6865eafaf811fe592197d6bcdcc5
sist <sist@sist27>
2025-08-18 10:30:07 +0900

    5

--- Chapter03_제어문(조건문,선택문)/src/제어문_조건문7.java
+++ Chapter03_제어문(조건문,선택문)/src/제어문_조건문7.java
@@ -8,11 +8,18 @@
  *   if(score<60)
  *     불합격 
  */
+import java.util.Scanner;
 public class 제어문_조건문7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		System.out.print("점수 입력:");
+		int score=scan.nextInt();
+		if (score<=100 && score>=60)
+			System.out.println("합격");
+		if (score<60 && score>=0)
+			System.out.println("불합격");
 	}
 
 }
