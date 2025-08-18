9e9bf5cddc2ad2ca6f97dfb5794cd7f1caf16362
sist <sist@sist27>
2025-08-18 16:52:08 +0900

    20

---
+++ Chapter08_클래스종류/src/com/sist/ast/GenieMusic.java
@@ -0,0 +1,7 @@
+package com.sist.ast;
+
+public class GenieMusic extends MusicClass{
+	public void musicAllData(){
+	System.out.println("지니뮤직 크롤링 완료");
+  }
+}
---
+++ Chapter08_클래스종류/src/com/sist/ast/Melon.java
@@ -0,0 +1,9 @@
+package com.sist.ast;
+
+public class Melon extends MusicClass{
+	//if => 어노테이션 -> index 역할
+@Override
+public void musicAllData() {
+	System.out.println("Melon 크롤링 완료");
+  }
+}
