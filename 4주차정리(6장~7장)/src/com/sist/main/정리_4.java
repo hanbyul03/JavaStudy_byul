0851456ea1be7e55a01bb2ae274d9090d66ba298
sist <sist@sist27>
2025-08-18 11:12:30 +0900

    19

---
+++ 4주차정리(6장~7장)/src/com/sist/main/정리_4.java
@@ -0,0 +1,32 @@
+package com.sist.main;
+import java.util.*;
+class GenieMusic{
+	public void dataIn()
+	{
+		System.out.println("지니 뮤직 크롤링");
+	}
+	public void list()
+	{
+		System.out.println("목록 출력!!");
+	}
+	public String titleFind(String fd)
+	{
+		System.out.println("제목으로 검색");
+		return fd;
+	}
+	// 가수 / 상세 / 동영상 
+}
+class Melon extends GenieMusic{
+	public void dataIn()
+	{
+		System.out.println("멜론뮤직 크롤링");
+	}
+}
+public class 정리_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	}
+
+}
